370902dbe5a27ea993ae4b2a3bc239a6b60e9d02
BEOMSU SHIM <rafa71802937@gmail.com>
2024-04-01 14:10:56 +0900

    Chore: Initialize project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by RAFA on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
