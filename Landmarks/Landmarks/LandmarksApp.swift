ac5b867d6e8e81c92749ac86e0e0e59b88dae484
BEOMSU SHIM <rafa71802937@gmail.com>
2024-04-02 17:48:16 +0900

    Chore: Initialize Project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by RAFA on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
