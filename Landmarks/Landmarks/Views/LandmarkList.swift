98455b0f130f0271383b5dbcf081c85f451a9e4d
BEOMSU SHIM <rafa71802937@gmail.com>
2024-04-03 23:14:53 +0900

    Feat: Complete Section 1 to 4

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by RAFA on 4/3/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
